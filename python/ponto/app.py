c4064d0b2ae32257be9e9cad303da6fb12d3d852
Smoothzz <thiago.rdoa@outlook.com>
2021-03-10 13:45:53 -0300

    subindo ponto

---
+++ python/ponto/app.py
@@ -0,0 +1,25 @@
+import time
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pause
+from datetime import datetime
+import getpass #lib que tras o hide password input
+import ctypes #lib que tras os comandos do windows
+
+url = 'http://ponto.paschoalotto.com.br/'
+
+passwd = getpass.getpass()
+pause.until(datetime(2020, 12, 7, 18, 1)) #Comando para realizar tarefa agendada
+browser = Chrome()
+browser.get(url)
+time.sleep(5)
+browser.find_element_by_id('username').send_keys('41972394835')
+browser.find_element_by_id ('password').send_keys(passwd)
+browser.find_element_by_id('btnLogin').click()
+time.sleep(5)
+browser.find_element_by_id('formMarc:j_idt59').click()
+time.sleep(5)
+browser.quit()
+time.sleep(2)
+ctypes.windll.user32.LockWorkStation() #comando para bloquear a tela do computador
