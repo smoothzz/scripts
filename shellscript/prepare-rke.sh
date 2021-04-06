da0ceb5eb7e4895a7bb71da8e6d105b5aedf6d55
smoothzz <53614138+smoothzz@users.noreply.github.com>
2021-04-06 12:51:53 -0300

    Add files via upload

---
+++ shellscript/prepare-rke.sh
@@ -0,0 +1,11 @@
+ufw disable
+swapoff -a; sed -i '/swap/d' /etc/fstab
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && apt install -y docker-ce docker-ce-cli containerd.io
