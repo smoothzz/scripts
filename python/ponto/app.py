e2aea72973032f4e4e64620746f81e0d38b40aca
Thiago Antunes <53614138+smoothzz@users.noreply.github.com>
2021-05-27 16:44:15 -0300

    Update app.py

--- python/ponto/app.py
+++ python/ponto/app.py
@@ -7,14 +7,14 @@ from datetime import datetime
 import getpass #lib que tras o hide password input
 import ctypes #lib que tras os comandos do windows
 
-url = 'https://ponto.paschoalotto.com.br'
+url = 'site'
 
 passwd = getpass.getpass()
 #pause.until(datetime(2020, 12, 7, 18, 1)) #Comando para realizar tarefa agendada
 browser = Chrome()
 browser.get(url)
 time.sleep(5)
-browser.find_element_by_id('username').send_keys('41972394835')
+browser.find_element_by_id('username').send_keys('cpf')
 browser.find_element_by_id ('password').send_keys(passwd)
 browser.find_element_by_id('btnLogin').click()
 time.sleep(5)
