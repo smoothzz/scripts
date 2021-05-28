09d67c869fcf46d6115aeb02f7b41f0ce73d8aad
Thiago Rodrigues De Oliveira Antunes <troantunes@paschoalotto.com.br>
2021-05-28 17:37:47 -0300

    k8s-cluster-file

--- python/ponto/app.py
+++ python/ponto/app.py
@@ -7,19 +7,19 @@ from datetime import datetime
 import getpass #lib que tras o hide password input
 import ctypes #lib que tras os comandos do windows
 
-url = 'https://ponto.paschoalotto.com.br'
+url = 'SITE'
 
 passwd = getpass.getpass()
 #pause.until(datetime(2020, 12, 7, 18, 1)) #Comando para realizar tarefa agendada
 browser = Chrome()
 browser.get(url)
 time.sleep(5)
-browser.find_element_by_id('username').send_keys('41972394835')
+browser.find_element_by_id('username').send_keys('CPF')
 browser.find_element_by_id ('password').send_keys(passwd)
 browser.find_element_by_id('btnLogin').click()
 time.sleep(5)
 browser.find_element_by_id('formMarc:j_idt59').click()
-time.sleep(5)
+time.sleep(2)
 browser.quit()
 #time.sleep(2)
-#ctypes.windll.user32.LockWorkStation() #comando para bloquear a tela do computador
+ctypes.windll.user32.LockWorkStation() #comando para bloquear a tela do computador
