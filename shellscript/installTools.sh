a18e1129a9f6a79118e256676de6e30153b8f387
smoothzz <thiago.rdoa@outlook.com>
2022-04-18 17:12:09 -0300

    add new files

--- shellscript/installTools.sh
+++ shellscript/installTools.sh
@@ -1,57 +1,50 @@
 !#/bin/bash
 
-## INSTALL TOOLS ##
+### KUBECTL ####
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-### install kubectl ####
-cd /tmp
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-mv kubectl /usr/bin
+### HELM ###
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
-### install helm ###
-cd /tmp
-curl -LO https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
-tar -zxvf helm-v3.2.1-linux-amd64.tar.gz
-chmod +x linux-amd64/helm
-mv linux-amd64/helm /usr/bin
+### TERRAFORM ###
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+### ULAUNCHER ###
+add-apt-repository ppa:agornostal/ulauncher -y
 
 ### install kubectx ###
 cd /tmp
 curl -LO https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz
 tar -zxvf kubectx_v0.9.4_linux_x86_64.tar.gz
 chmod +x kubectx
-mv kubectx /usr/bin
+mv kubectx /usr/local/bin
 
 ### install kubens ###
 curl -LO https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz
 tar -zxvf kubens_v0.9.4_linux_x86_64.tar.gz
 chmod +x kubens
-mv kubens /usr/bin
+mv kubens /usr/local/bin
 
-### install terraform ###
-wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip
-unzip terraform_1.1.2_linux_amd64.zip
-mv terraform /usr/bin
+# ### install k8s aliases ###
+# wget -P /home/$USER https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
+# echo 'source ~/.kubectl_aliases' >> /home/$USER/.zshrc
 
-### install k8s aliases ###
-wget -P /home/$USER https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
-echo 'source ~/.kubectl_aliases' >> /home/$USER/.zshrc
-
-### install visual studio code ###
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https -y
-sudo apt update -y
-sudo apt install code -y
+### VISUAL CODE ###
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add –
+add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
 
 ### install vagrant ####
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && sudo apt-get install vagrant -y
+
+## INSTALL TOOLS ##
+apt update
+apt install -y apt-transport-https ca-certificates gnupg software-properties-common curl git tilix vim flameshot zsh kubectl helm terraform ulauncher vagrant code
+
+### OhMyZSH
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Tools have been installed successful"
-
-
-
---
+++ shellscript/setup-machine.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# HASHCORP
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# MICROSOFT
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+# KUBECTL
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# HELM
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+# DOCKER
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# KIND
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.12.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin
+
+# INSTALL
+apt update -y
+apt install -y gnupg ca-certificates apt-transport-https software-properties-common curl git wget vim python3-pip python3-venv tilix flameshot zsh kubectl helm ulauncher vagrant unzip gzip virtualbox virtualbox-ext-pack docker-ce docker-ce-cli containerd.io terraform code
+apt clean
